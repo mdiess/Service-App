54b50bf739f11c3b3a75feaf119c3799bd056fa0
Max Diess <max.diess27@gmail.com>
2021-11-16 18:30:21 -0800

    Initial Commit

---
+++ Final/FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalApp.swift
+//  Final
+//
+//  Created by Max Diess on 11/16/21.
+//
+
+import SwiftUI
+
+@main
+struct FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
